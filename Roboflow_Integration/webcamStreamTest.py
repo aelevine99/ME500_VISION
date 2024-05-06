8c5bc4d808c09870d87d4632ba19336fed89e161
Matt Cowley <144645078+mattcowley10@users.noreply.github.com>
2024-05-06 18:08:38 -0400

    Update webcamStreamTest.py

--- Roboflow_Integration/webcamStreamTest.py
+++ Roboflow_Integration/webcamStreamTest.py
@@ -1,4 +1,4 @@
-#I found this online, and it make be faster than making individual calls to the API
+#I found this online, and it may be faster than making individual calls to the API. Still worth looking into if you you'd like to optimize the Roboflow methods
 from inference import InferencePipeline
 from inference.core.interfaces.stream.sinks import render_boxes
 
